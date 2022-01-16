9eb2e902ca5a6babd4ca9e2327219b62bf94bfb5
Henrique Tofoli <henrique.188@outlook.com>
2022-01-16 17:14:09 -0300

    Atualizar elemento script

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
 const imagem = document.querySelector('img');
-const botao = document.querySelector('#botao');
+const botao = document.getElementById('#botao');
 const nomeDoPersonagem = document.querySelector('#nome');
 const especie = document.querySelector('#especie');
 const condicao = document.querySelector('#status');
 
 pegarPersonagem = () => {
-    return fetch('https://rickandmortyapi.com/api/character/2', {
+    return fetch(`https://rickandmortyapi.com/api/character/2`, {
         method:'GET',
         headers: {
             Accept: 'application/json',
             "Content-type": 'application/json'
         }
     }).then((response) => response.json()).then((data) => {
+        console.log(data)
         imagem.src = data.image;
         imagem.alt = data.name;
         nomeDoPersonagem.innerHTML = data.name;
