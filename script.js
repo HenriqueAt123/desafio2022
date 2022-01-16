9ff6cdfe41cdb76a8f1e55c223ad72ed2ba5f989
Henrique Tofoli <henrique.188@outlook.com>
2022-01-16 19:42:51 -0300

    Finalizar desafio

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ gerarValorAleatorio = () => {
 
 pegarPersonagem = () => {
     let numeroAleatorio = gerarValorAleatorio();
-    return fetch(`https://rickandmortyapi.com/api/character/${numeroAleatorio}`, {
+     fetch(`https://rickandmortyapi.com/api/character/${numeroAleatorio}`, {
         method:'GET',
         headers: {
             Accept: 'application/json',
@@ -24,6 +24,46 @@ pegarPersonagem = () => {
         especie.innerHTML = data.species;
         condicao.innerHTML = data.status;
     });
+
+   numeroAleatorio = gerarValorAleatorio();
+     fetch(`https://rickandmortyapi.com/api/character/${numeroAleatorio}`, {
+        method:'GET',
+        headers: {
+            Accept: 'application/json',
+            "Content-type": 'application/json'
+        }
+    }).then((response) => response.json()).then((data) => {
+         const imagem2 = document.querySelector('#imagem2');
+         const nomeDoPersonagem2 = document.querySelector('#nome2');
+         const especie2 = document.querySelector('#especie2');
+         const condicao2 = document.querySelector('#status2');
+        imagem2.src = data.image;
+        imagem2.alt = data.name;
+        nomeDoPersonagem2.innerHTML = data.name;
+        especie2.innerHTML = data.species;
+        condicao2.innerHTML = data.status;
+    });
+
+    numeroAleatorio = gerarValorAleatorio();
+    return fetch(`https://rickandmortyapi.com/api/character/${numeroAleatorio}`, {
+        method:'GET',
+        headers: {
+            Accept: 'application/json',
+            "Content-type": 'application/json'
+        }
+    }).then((response) => response.json()).then((data) => {
+         const imagem3 = document.querySelector('#imagem3');
+         const nomeDoPersonagem3 = document.querySelector('#nome3');
+         const especie3 = document.querySelector('#especie3');
+         const condicao3 = document.querySelector('#status3');
+        imagem3.src = data.image;
+        imagem3.alt = data.name;
+        nomeDoPersonagem3.innerHTML = data.name;
+        especie3.innerHTML = data.species;
+        condicao3.innerHTML = data.status;
+    });
+
+
 }
 
 botao.onclick = pegarPersonagem;
